2934df2d66144594bfa8762ca5fa71c7d228e323
quamilou <quamilougame@gmail.com>
2023-10-31 10:25:26 +0100

    UPDATED | Projet 1

--- Home.php
+++ Home.php
@@ -1,7 +1,17 @@
 <html>
     <head>
-        <h1>Home</h1>
-    </head>
+    <link rel="stylesheet" href="assert/style.css">
+  <body>
+    <header>
+      <h1>Mon Portfolio</h1>
+      <nav>
+        <ul>
+          <li><a href= >Accueil</a></li>
+          <li><a href= >Epreuve E4</a></li>
+          <li><a href= >Epreuve E5</a></li>
+        </ul>
+      </nav>
+    </header>
     <body>
         <h3>Hello !!!</h3>
 <?php
@@ -22,18 +32,24 @@ try {
     
     $requete = $connexion->prepare("SELECT * FROM client WHERE nom = '$nom' AND mdp = '$mdp'");
     $requete->execute();
+    
     $resultats = $requete->fetchAll(PDO::FETCH_ASSOC);
 
+    
     foreach ($resultats as $utilisateur) {  
-          
-        echo $utilisateur['nom'].'</br>';
-        echo $utilisateur['prenom'].'</br>';
-        echo $utilisateur['age'].'</br>';
-        echo $utilisateur['poids'].'</br>';
-        echo $utilisateur['taille'].'</br>';
-        echo $utilisateur['sexe'].'</br>';
-        echo $utilisateur['IMC'].'</br>';
-       
+
+        if($nom == $utilisateur['nom'] && $mdp == $utilisateur['mdp'] ){
+                        
+            echo $utilisateur['nom'].'</br>';
+            echo $utilisateur['prenom'].'</br>';
+            echo $utilisateur['age'].'</br>';
+            echo $utilisateur['poids'].'</br>';
+            echo $utilisateur['taille'].'</br>';
+            echo $utilisateur['sexe'].'</br>';
+            echo $utilisateur['IMC'].'</br>';
+        }else{
+            echo 'Le nom '.$nom.' ou mot de passe NOT CORRECT '.'</br>';
+        }
     }
 ?>  
 
