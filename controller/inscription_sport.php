5b6f1455fee1303c52085b754b3254599446f39e
quamilou <quamilougame@gmail.com>
2024-03-06 22:59:03 +0100

    Update inscription_sport.php

--- controller/inscription_sport.php
+++ controller/inscription_sport.php
@@ -1,38 +1,36 @@
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Error_inscription</title>
+        <link rel="stylesheet" href="../assert/style.css">
+    </head>
+    <body>
+        <main>
+        <form class="alllogin" action="../vue/Home.php" method="POST"> 
+            <h2>Error_inscription</h2>
 <?php
 require "../model/CBDD.php";
 session_start();
 
-$idClient = $_SESSION['user']['Id_Client'];
-$idSport = $_POST['idSport'];
-
-// Vérifie si l'ID du sport et l'ID de l'utilisateur sont disponibles
-if (isset($_POST['idSport']) && isset($_SESSION['user']['Id_Client'])) {
-    $idSport = $_POST['idSport'];
+if (!empty($_POST['idSport']) && is_array($_POST['idSport'])) {
     $idClient = $_SESSION['user']['Id_Client'];
-
-    // Vérifier si l'utilisateur est déjà inscrit à ce sport
-    $stmt = $pdo->prepare("SELECT COUNT(*) FROM pratique WHERE Id_Client = :idClient AND Id_Sport = :idSport");
-    $stmt->execute(['idClient' => $idClient, 'idSport' => $idSport]);
-    $dejaInscrit = $stmt->fetchColumn() > 0;
-
-    // Insérer l'inscription dans la table pratique
-    $stmt = $pdo->prepare("INSERT INTO pratique (Id_Client, Id_Sport) VALUES (:idClient, :idSport)");
-    $stmt->execute(['idClient' => $idClient, 'idSport' => $idSport]);
-
-
-    if (!$dejaInscrit) {
-        // Inscrire l'utilisateur au sport
-        $stmt = $pdo->prepare("INSERT INTO pratique (Id_Client, Id_Sport) VALUES (:idClient, :idSport)");
-        $stmt->execute(['idClient' => $idClient, 'idSport' => $idSport]);
-        // Rediriger vers la page des sports avec un message de succès
-        header("Location: ../vue/Sport.php?inscriptionReussie=1");
-    } else {
-        // L'utilisateur est déjà inscrit à ce sport, gérer cette situation
-        header("Location: ../vue/Sport.php?dejaInscrit=1");
+    
+    foreach ($_POST['idSport'] as $idSport) {
+        $checkInscription = $pdo->prepare("SELECT COUNT(*) FROM pratique WHERE Id_Client = :idClient AND Id_Sport = :idSport");
+        $checkInscription->execute(['idClient' => $idClient, 'idSport' => $idSport]);
+        
+        if ($checkInscription->fetchColumn() == 0) {
+            $stmt = $pdo->prepare("INSERT INTO pratique (Id_Client, Id_Sport) VALUES (:idClient, :idSport)");
+            $stmt->execute(['idClient' => $idClient, 'idSport' => $idSport]);
+        }
     }
+
+    header("Location: ../vue/Sport.php?success=1");
+    exit;
 } else {
-    // Rediriger vers la page des sports avec un message d'erreur
-    header("Location: ../vue/Sport.php?erreurInscription=1");
+    //header("Location: Sport.php?error=1");
+    //exit;
+    echo "Aucune sport n'est sélectionnée";
 }
-exit;
 ?>
+
