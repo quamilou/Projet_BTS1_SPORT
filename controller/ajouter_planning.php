e4312bc876445f3e0511014349892d2fe39bb2e2
quamilou <quamilougame@gmail.com>
2024-03-07 00:19:05 +0100

    Create ajouter_planning.php

---
+++ controller/ajouter_planning.php
@@ -0,0 +1,19 @@
+<?php
+require "../model/CBDD.php";
+session_start();
+
+$idClient = $_POST['idClient'];
+$titre = $_POST['titre'];
+$nom_planning = $_POST['nom_planning'];
+$date_heure = $_POST['date_heure'];
+$duree = $_POST['duree'];
+
+$stmt = $pdo->prepare("INSERT INTO planning (titre, date_heure, nom_planning, durée, Id_Client) VALUES (:titre, :date_heure, :nom_planning, :duree, :idClient)");
+$stmt->execute(['titre' => $titre, 'date_heure' => $date_heure, 'nom_planning' => $nom_planning, 'duree' => $duree, 'idClient' => $idClient]);
+
+
+
+// Redirection vers la page de planning avec un message de confirmation
+header("Location: ../vue/Planning.php?success=1");
+exit;
+?>
