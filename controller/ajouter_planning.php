f9be6e85e9c0d476af0de4817a7fe98a1f6ee7b6
quamilou <quamilougame@gmail.com>
2024-05-31 21:47:31 +0200

    Update ajouter_planning.php

--- controller/ajouter_planning.php
+++ controller/ajouter_planning.php
@@ -2,8 +2,13 @@
 require "../model/CBDD.php";
 session_start();
 
-$utilisateurId = $_SESSION['user']['Id_Client'];
-$stmt = ("SELECT * FROM client");
+// Vérifie si l'ID du client est défini dans la session
+if (isset($_SESSION['user']['Id_Client'])) {
+    $idClient = $_SESSION['user']['Id_Client'];
+} else {
+    die('Erreur : l\'ID du client n\'est pas défini.');
+}
+
 $titre = $_POST['titre'];
 $nom_planning = $_POST['nom_planning'];
 $date_heure = $_POST['date_heure'];
@@ -11,8 +16,6 @@ $duree = $_POST['duree'];
 
 $stmt = $pdo->prepare("INSERT INTO planning (titre, date_heure, nom_planning, durée, Id_Client) VALUES (:titre, :date_heure, :nom_planning, :duree, :idClient)");
 $stmt->execute(['titre' => $titre, 'date_heure' => $date_heure, 'nom_planning' => $nom_planning, 'duree' => $duree, 'idClient' => $idClient]);
-
-
 
 // Redirection vers la page de planning avec un message de confirmation
 header("Location: ../vue/Planning.php?success=1");
